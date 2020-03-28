388ec981b12e9a6824f483d10c3c3cb7d996791c
fadhilahpuspasari <fadhilahpuspasari.48@gmail.com>
2020-03-28 16:55:36 +0700

    pertama

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,7 +12,7 @@ class TaskController extends Controller
     public function prime(){
         $num=1;
         $data['prime'] = [];
-        while($num<100){
+        while($num<1000){
             $n = isPrime($num);
             if($n!=null){
                 $data['prime'][] = $n ;
